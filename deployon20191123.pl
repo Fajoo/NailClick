9a4071dab78b29742d4e287b86be0a3c277be91f
Dima Savchenko <dimasav57@gmail.com>
2020-02-05 14:22:16 +0300

    1.1

--- deployon20191123.pl
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Content-type: text/plain\n";
-echo '';
-cd /var/www/domains/g2-public.qzo.su/ || exit > /dev/null
-git reset --hard > /dev/null
-git pull ssh://gafurovstudio@bitbucket.org/gafurovstudio/g2-public.git develop > /dev/null
-#git@bitbucket.org:gafurovstudio/g2.git
