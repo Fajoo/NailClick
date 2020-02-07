117d68e3df3393f9d99ebd48ef84bbaa2964aeb1
Dima Savchenko <dimasav57@gmail.com>
2020-02-07 13:48:11 +0300

    1.7

--- deploy.pl
+++ deploy.pl
@@ -3,5 +3,5 @@ echo "Content-type: text/plain\n";
 echo '';
 cd /var/www/domains/st.nailclick.qzo.su/ || exit > /dev/null
 git reset --hard > /dev/null
-git pull
+git pull > /dev/null
 https://github.com/Fajoo/NailClick  > /dev/null
